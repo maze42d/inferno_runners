7870475be204a13364678e52c26c454c90d40f6a
Roxie Deards <maze@maze42.eu>
2025-06-18 13:33:51 +0100

    more modularity

---
+++ config/inferno.conf.sh
@@ -0,0 +1,9 @@
+# shellcheck disable=SC2148
+# see https://github.com/teodly/inferno#settings
+# remember the INFERNO_ prefix
+export INFERNO_NAME="inferno"
+export INFERNO_RX_CHANNELS=2
+export INFERNO_TX_CHANNELS=2
+export INFERNO_SAMPLE_RATE=48000
+export INFERNO_RX_LATENCY_NS=1000000
+export INFERNO_TX_LATENCY_NS=1000000
