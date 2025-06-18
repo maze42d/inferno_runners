55eef845dde1c09a357735f1cb8ea06584af5d30
Roxie Deards <maze@maze42.eu>
2025-06-18 14:41:20 +0100

    workaround

--- config/inferno.conf.sh
+++ config/inferno.conf.sh
@@ -1,9 +1,12 @@
 # shellcheck disable=SC2148
+
 # see https://github.com/teodly/inferno#settings
 # remember the INFERNO_ prefix
+
 export INFERNO_NAME="inferno"
-export INFERNO_RX_CHANNELS=2
-export INFERNO_TX_CHANNELS=2
+## bug in inferno; accept env for tx but not rx; set in alsa.conf
+#export INFERNO_RX_CHANNELS=2
+#export INFERNO_TX_CHANNELS=2
 export INFERNO_SAMPLE_RATE=48000
 export INFERNO_RX_LATENCY_NS=1000000
 export INFERNO_TX_LATENCY_NS=1000000
