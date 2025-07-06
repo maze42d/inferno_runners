a06f171971ea96d534c5add251211d1174fa51c6
Roxie Deards <maze@maze42.eu>
2025-07-06 02:35:41 +0100

    remove unneeded nodes

--- run_pipewire_linker.sh
+++ run_pipewire_linker.sh
@@ -1,5 +1,33 @@
 # shellcheck disable=SC2148
 
+target_nodes=("Dummy-Driver" "Freewheel-Driver" "Midi-Bridge" "hdmi")
+
+contains() {
+  local e
+  for e in "${target_nodes[@]}"; do
+    [[ "$e" == "$1" ]] && return 0
+  done
+  return 1
+}
+
+current_id=""
+while IFS= read -r line; do
+  # Match line with id
+  if [[ $line =~ ^[[:space:]]*id[[:space:]]+([0-9]+), ]]; then
+    current_id="${BASH_REMATCH[1]}"
+  fi
+  # Match line with node.name
+  if [[ $line =~ node\.name[[:space:]]*=[[:space:]]*\"([^\"]+)\" ]]; then
+    node_name="${BASH_REMATCH[1]}"
+    if contains "$node_name"; then
+      echo "Destroying node id $current_id with name $node_name"
+      pw-cli destroy "$current_id"
+    fi
+  fi
+done < <(pw-cli ls Node)
+
+
+
 cd pw_links || { echo "Error: Could not find the pw_links directory"; exit 1; }
 
 if [ ! -f active ]; then
